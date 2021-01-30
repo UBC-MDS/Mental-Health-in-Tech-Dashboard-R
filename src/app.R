7ecb39ac71d0d3ca3c80be109ba3555bb52cb159
fatse <fatimeseelimi@gmail.com>
2021-01-29 16:36:04 -0800

    Add benefit plots

--- src/app.R
+++ src/app.R
@@ -137,9 +137,86 @@ app$layout(
               dccGraph(id = "remote_barplot")
             )
           )
-        ))
+        )
+      ),
+      htmlHr(),
+      dbcRow(
+        list(
+          dbcCol(
+            list(
+              htmlP("Has your employer ever formally discussed mental health (for example, as part of a wellness campaign or other official communication)?"),
+              dccRadioItems(
+                id = "formal_discuss_radio",
+                options = list(
+                  list(label = "Yes", value = "Yes"),
+                  list(label = "No", value = "No"),
+                  list(label = "I don't know", value = "I don't know")
+                ),
+                value = "Yes",
+                labelStyle = list("display" = "block"),
+                inputStyle = list("marginLeft" = "20px", "marginRight" = "5px")
+              )
+            )
+          ),
+          dbcCol(
+            list(
+              htmlP("Do you know the options for mental health care available under your employer-provided coverage?"),
+              dccRadioItems(
+              id = "mental_health_benefits_employer_radio",
+              options = list(
+                list(label = "Yes", value = "Yes"),
+                list(label = "No", value = "No"),
+                list(label = "I am not sure", value = "I am not sure")
+              ),
+              value = "Yes",
+              labelStyle = list("display" = "block"),
+              inputStyle = list("marginLeft" = "20px", "marginRight" = "5px")
+              )
+            )
+          ),
+          dbcCol(
+            list(
+              htmlP("If a mental health issue prompted you to request a medical leave from work, asking for that leave would be:"),
+                dccRadioItems(
+                id = "mental_health_leave_radio",
+                options = list(
+                list(label = "Very easy", value = "Very easy"),
+                list(label = "Somewhat easy", value = "Somewhat easy"),
+                list(label = "Neither easy nor difficult", value = "Neither easy nor difficult"),
+                list(label = "Somewhat difficult", value = "Somewhat difficult"),
+                list(label = "Very difficult", value = "Very difficult")
+              ),
+            value = "Very easy",
+            labelStyle = list("display" = "block"),
+            inputStyle = list("marginLeft" = "20px", "marginRight" = "5px")
+          )
+        )
+      ),
+      dbcRow(
+        list(
+          dbcCol(
+            list(
+              dccGraph(id = "formal_discuss_donutplot")
+            )
+          ),
+          dbcCol(
+            list(
+              dccGraph(id = "mental_health_benefits_employer_donutplot")
+            )
+          ),
+          dbcCol(
+            list(
+              dccGraph(id = "mental_health_leave_donutplot")
+            )
+          )
+        )
+      )
     )
-  ))
+  )
+)
+)
+)
+
 
 
 app$callback(
@@ -228,5 +305,80 @@ app$callback(
     
     ggplotly(plot3)
   })
+
+
+
+app$callback(
+  output('formal_discuss_donutplot', 'figure'),
+  list(input('formal_discuss_radio', 'value')),
+  function(radiovalue) {
+    build_graph(formal_discuss, radiovalue)
+  }
+)
+
+
+app$callback(
+  output('mental_health_benefits_employer_donutplot', 'figure'),
+  list(input('mental_health_benefits_employer_radio', 'value')),
+  function(radiovalue) {
+    build_graph(mental_health_benefits_employer, radiovalue)
+  }
+)
+
+
+app$callback(
+  output('mental_health_leave_donutplot', 'figure'),
+  list(input('mental_health_leave_radio', 'value')),
+  function(radiovalue="Very easy") {
+    build_graph(mental_health_leave, radiovalue)
+  }
+)
+
+
+#' Donut chart using plotly
+#'
+#' Creates a donut chart gpraph using plotly library 
+#'
+#' @param column_name quoted column name to be used for the plot
+#' @param column_input value of input coming from the callback function
+#'
+#' @return plotly layout
+#'
+#' @examples
+#' build_graph(formal_discuss, "Yes")
+#'
+#' @export
+build_graph <- function(column_name, column_input) {
+  column_name <- enquo(column_name)
+  
+  subset_data <-
+    data %>%
+    select(!!column_name, country) %>%
+    mutate(clean_country = ifelse(country %in% countries, country, 'Other'))  %>%
+    select(-country) %>%
+    mutate_all(funs(na_if(., ""))) %>%
+    remove_empty("cols") %>%
+    drop_na()
+  
+  normalize_counts <-
+    subset_data %>% group_by(clean_country) %>% count(!!column_name) %>%
+    transform(n = (n / sum(n) * 100)) %>%
+    spread(key = !!column_name, value = n)
+  
+  labs <- normalize_counts$clean_country
+  vals <- normalize_counts[[column_input]]
+  
+  plot_ly(normalize_counts,
+          labels = ~ labs,
+          values = ~ vals) %>%
+    add_pie(hole = 0.44) %>%
+    layout(legend = list(x = 0.01, y = 0.99, yanchor="bottom", xanchor="left"),
+           margin=list(r=20, l=0, b=0, t=0),
+           autosize=FALSE,
+           width = 330,
+           height=330
+           )
+  
+}
 
 app$run_server(debug = T)
